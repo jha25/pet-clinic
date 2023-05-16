ef45e209ec1a1ff0e393d414506a96e4aca6ee77
Ha <630019@bah.com>
2023-05-16 17:39:35 -0500

    refactor services

--- pet-clinic-data/src/main/java/com/jason/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/com/jason/petclinic/services/PetService.java
@@ -2,10 +2,6 @@ package com.jason.petclinic.services;
 
 import com.jason.petclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/com/jason/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/jason/petclinic/services/VetService.java
@@ -1,12 +1,7 @@
 package com.jason.petclinic.services;
 
-import com.jason.petclinic.model.Owner;
 import com.jason.petclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
